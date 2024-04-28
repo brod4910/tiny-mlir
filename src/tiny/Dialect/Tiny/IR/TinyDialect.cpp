e5d8db169178d5eab16ec8946ec33243fd3b197c
brod4910 <brod4910@gmail.com>
2024-04-28 17:19:35 -0600

    partial impl for constant materializer

--- src/tiny/Dialect/Tiny/IR/TinyDialect.cpp
+++ src/tiny/Dialect/Tiny/IR/TinyDialect.cpp
@@ -1,6 +1,10 @@
 #include "TinyDialect.h"
 #include "TinyOps.h"
 #include "mlir/IR/Builders.h"
+#include "mlir/IR/BuiltinAttributes.h"
+#include "mlir/IR/BuiltinTypes.h"
+
+#include "mlir/IR/Types.h"
 #include "llvm/ADT/TypeSwitch.h"
 
 #include "tiny/Dialect/Tiny/IR/TinyDialect.cpp.inc"
@@ -16,4 +20,15 @@ void TinyDialect::initialize() {
       >();
 }
 
+Operation *TinyDialect::materializeConstant(OpBuilder &builder, Attribute value,
+                                            Type type, Location loc) {
+  auto tensor_type = dyn_cast<TensorType>(type);
+  auto const_value = dyn_cast<DenseElementsAttr>(value);
+
+  if (!tensor_type || !const_value) {
+    return nullptr;
+  }
+
+  return builder.create<ConstantOp>(loc, type, const_value);
+}
 } // namespace mlir::tiny
--- tests/unary_ops.mlir
+++ tests/unary_ops.mlir
@@ -1,4 +1,10 @@
 // RUN: tiny-opt %s --tiny-erase-noop --tiny-cast-noop | FileCheck %s
+tiny.func @simple_constant() -> tensor<3xf32> {
+    %0 = tiny.constant dense<[1.0, 2.0, 3.0]> : tensor<3xf32>
+
+    return %0 : tensor<3xf32>
+}
+
 tiny.func @simple_exp2() -> tensor<3xf32> {
     %0 = arith.constant dense<[1.0, 2.0, 3.0]> : tensor<3xf32>
     %1 = tiny.exp2 %0 : tensor<3xf32> -> tensor<3xf32>
